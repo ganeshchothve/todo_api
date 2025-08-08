837c59ff6dd7a2a52b0b2017fe053cea1a00ab88
ganeshchothve <ganeshsantosh8390@gmail.com>
2025-08-08 20:01:14 +0530

    add todo backend controller

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class TodosController < ApplicationController
+      before_action :set_todo, only: %i[show update destroy]
+
+      def index
+        todos = Todo.order(created_at: :desc)
+        render json: todos
+      end
+
+      def show
+        render json: @todo
+      end
+
+      def create
+        todo = Todo.new(todo_params)
+        if todo.save
+          render json: todo, status: :created
+        else
+          render json: { errors: todo.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @todo.update(todo_params)
+          render json: @todo
+        else
+          render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @todo.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_todo
+        @todo = Todo.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Todo not found" }, status: :not_found
+      end
+
+      def todo_params
+        params.require(:todo).permit(:title, :description, :completed)
+      end
+    end
+  end
+end
